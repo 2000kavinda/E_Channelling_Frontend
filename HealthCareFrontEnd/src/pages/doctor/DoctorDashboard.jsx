77e9b69fac1e86f562ef01c1de4cb158796c9c96
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2024-08-14 00:40:14 +0530

    Today Appointment Scroll down lists create

--- HealthCareFrontEnd/src/pages/doctor/DoctorDashboard.jsx
+++ HealthCareFrontEnd/src/pages/doctor/DoctorDashboard.jsx
@@ -1,4 +1,16 @@
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaQuestion } from "react-icons/fa6";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BsPeople } from "react-icons/bs";
+import { AiOutlineFieldTime } from "react-icons/ai";
+import { BiCabinet } from "react-icons/bi";
+import { useRef } from 'react';
+import DoctorPicture from '../../assets/Images/Ellipse34.png';
+
 function DoctorDashboard() {
+
+  const divRef = useRef(null);
+  const bottomRef = useRef(null);
 
   const getCurrentGreeting = () => {
     const currentHour = new Date().getHours();
@@ -15,12 +27,212 @@ function DoctorDashboard() {
   return (
     <div className="flex flex-col px-10 pt-10">
       {/* Top bar */}
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between w-full">
         {/* Greeting message */}
         <div className="flex flex-col">
-          <div className="text-2xl font-bold text-[#00394C]">{getCurrentGreeting()}</div>
+          <div className="text-4xl font-bold text-[#00394C]">{getCurrentGreeting()}</div>
+          {/* Dr name */}
+          <div className="text-base text-[#414141] font-semibold">Dr.Hiran Welagedara</div>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-row gap-10">
+        {/* Single Button */}
+          <button className="flex flex-row items-center gap-2">
+            <div className="w-8 h-8 rounded-full bg-[#e0e0e0] flex flex-col items-center justify-center"><IoNotificationsOutline className="w-5 h-5 text-[#00394C]"/></div>
+            <div className="text-base font-semibold text-[#00394C]">Alerts</div>
+          </button>
+
+          {/* Single Button */}
+          <button className="flex flex-row items-center gap-2">
+            <div className="w-8 h-8 rounded-full bg-[#e0e0e0] flex flex-col items-center justify-center"><FaQuestion className="w-5 h-5 text-[#00394C]" /></div>
+            <div className="text-base font-semibold text-[#00394C]">help</div>
+          </button>
+
+          {/* Single Button */}
+          <button className="flex flex-row items-center gap-2">
+            <div className="w-8 h-8 rounded-full bg-[#e0e0e0] flex flex-col items-center justify-center"><IoSettingsOutline className="w-5 h-5 text-[#00394C]" /></div>
+            <div className="text-base font-semibold text-[#00394C]">settings</div>
+          </button>
+        </div>
+        
+      </div>
+
+
+
+      {/* Card Section */}
+      <div className="flex flex-row w-full gap-8 pt-10">
+
+      {/* Card */}
+        <div className="flex flex-row bg-[#E6F2F6] w-1/3 h-[150px] rounded-lg items-center px-5 gap-5">
+          <div className="w-[70px] h-[70px] bg-[#00394C] rounded-lg justify-center items-center flex flex-col">
+            <BsPeople className="w-8 h-8 text-white"/>
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-1/2 h-full gap-2">
+            <div className="font-semibold text-[#414141]">Total Appointment</div>
+            <div className="text-4xl font-bold text-[#414141}">120</div>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div className="flex flex-row bg-[#E6F2F6] w-1/3 h-[150px] rounded-lg items-center px-5 gap-5">
+          <div className="w-[70px] h-[70px] bg-[#00394C] rounded-lg justify-center items-center flex flex-col">
+            <BiCabinet className="w-8 h-8 text-white" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-1/2 h-full gap-2">
+            <div className="font-semibold text-[#414141]">Total Rooms Count</div>
+            <div className="text-4xl font-bold text-[#414141}">120</div>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div className="flex flex-row bg-[#E6F2F6] w-1/3 h-[150px] rounded-lg items-center px-5 gap-5">
+          <div className="w-[70px] h-[70px] bg-[#00394C] rounded-lg justify-center items-center flex flex-col">
+            <AiOutlineFieldTime className="text-white w-9 h-9" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-1/2 h-full gap-2">
+            <div className="font-semibold text-[#414141]">Total Time Count</div>
+            <div className="text-4xl font-bold text-[#414141}">120</div>
+          </div>
         </div>
       </div>
+
+
+
+      {/* Content rows */}
+      <div className="flex flex-row w-full h-full gap-6 pt-10">
+
+        {/* Today appointment */}
+      <div className="flex flex-col w-1/2 gap-4">
+          <div className="text-base font-semibold text-[#414141]">Today Appointment Amounts</div>
+
+        <div className="flex flex-col  h-full bg-[#E6F2F6] rounded-xl px-4 py-4">
+          <div
+            ref={divRef}
+            style={{ overflowY: 'scroll', height: '320px' }}
+          >
+
+          {/* Patients Cards */}
+            <div className="h-[100px] w-full bg-white flex flex-row items-center px-4 rounded-xl justify-between mb-2">
+            {/* Doctor Picture */}
+                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
+                  <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
+                </div>
+                {/* Patients Details */}
+                <div className="flex flex-col w-2/3 pl-2">
+                  <div className="text-lg font-semibold text-[#414141]">Mr.Hiran Welagedara</div>
+                  <div className="text-sm text-[#414141]">No.02</div>
+                  <div className="flex flex-row gap-4">
+                    <div className="flex flex-row text-sm text-[#414141]">
+                    <div>Age</div>
+                    <div>22</div>
+                  </div>
+                  <div className="flex flex-row text-sm">
+                    <div>Gender:</div>
+                    <div>Male</div>
+                  </div>
+                  </div>
+                </div>
+
+                <div>12.00pm</div>
+            </div>
+
+
+              {/* Patients Cards */}
+              <div className="h-[100px] w-full bg-white flex flex-row items-center px-4 rounded-xl justify-between mb-2">
+                {/* Doctor Picture */}
+                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
+                  <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
+                </div>
+                {/* Patients Details */}
+                <div className="flex flex-col w-2/3 pl-2">
+                  <div className="text-lg font-semibold text-[#414141]">Mr.Hiran Welagedara</div>
+                  <div className="text-sm text-[#414141]">No.02</div>
+                  <div className="flex flex-row gap-4">
+                    <div className="flex flex-row text-sm text-[#414141]">
+                      <div>Age</div>
+                      <div>22</div>
+                    </div>
+                    <div className="flex flex-row text-sm">
+                      <div>Gender:</div>
+                      <div>Male</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div>12.00pm</div>
+              </div>
+
+
+              {/* Patients Cards */}
+              <div className="h-[100px] w-full bg-white flex flex-row items-center px-4 rounded-xl justify-between mb-2">
+                {/* Doctor Picture */}
+                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
+                  <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
+                </div>
+                {/* Patients Details */}
+                <div className="flex flex-col w-2/3 pl-2">
+                  <div className="text-lg font-semibold text-[#414141]">Mr.Hiran Welagedara</div>
+                  <div className="text-sm text-[#414141]">No.02</div>
+                  <div className="flex flex-row gap-4">
+                    <div className="flex flex-row text-sm text-[#414141]">
+                      <div>Age</div>
+                      <div>22</div>
+                    </div>
+                    <div className="flex flex-row text-sm">
+                      <div>Gender:</div>
+                      <div>Male</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div>12.00pm</div>
+              </div>
+
+
+              {/* Patients Cards */}
+              <div className="h-[100px] w-full bg-white flex flex-row items-center px-4 rounded-xl justify-between mb-2">
+                {/* Doctor Picture */}
+                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
+                  <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
+                </div>
+                {/* Patients Details */}
+                <div className="flex flex-col w-2/3 pl-2">
+                  <div className="text-lg font-semibold text-[#414141]">Mr.Hiran Welagedara</div>
+                  <div className="text-sm text-[#414141]">No.02</div>
+                  <div className="flex flex-row gap-4">
+                    <div className="flex flex-row text-sm text-[#414141]">
+                      <div>Age</div>
+                      <div>22</div>
+                    </div>
+                    <div className="flex flex-row text-sm">
+                      <div>Gender:</div>
+                      <div>Male</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div>12.00pm</div>
+              </div>
+
+            <div ref={bottomRef}></div>
+          </div>
+        </div>
+        </div>
+
+
+
+        <div className="flex flex-col w-1/2 h-full gap-4">
+          <div className="text-base font-semibold text-[#414141]">Today Appointment Amounts</div>
+          <div className="w-full h-[350px] bg-[#E6F2F6] rounded-xl"></div>
+        
+        </div>
+      </div>
+
+
     </div>
   )
 }
