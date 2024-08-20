fc8b57e7150bf46d62b5fca70bc00b8f46829b4e
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2024-08-21 00:01:39 +0530

    firebase added to the project

--- HealthCareFrontEnd/src/App.jsx
+++ HealthCareFrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import NavBar from './components/header/NavBar'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import HomePage from './pages/home/Home';
+import DrAdmin from './pages/admin/DrAdmin';
+
 
 function App() {
 
@@ -9,8 +11,10 @@ function App() {
     <div>
       <Router>
       <Routes>
-        <Route exact path='/' element={<NavBar/>}/>
+        
+        <Route exact path='/' element={<DrAdmin/>}/>
         <Route path='/' element={<HomePage />} />
+        <Route path='/DrAdmin' element={<DrAdmin/>}/>
       </Routes>
 
       </Router>
---
+++ HealthCareFrontEnd/src/pages/admin/DrAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DrAdmin() {
+  return (
+    <div>DrAdmin</div>
+  )
+}
+
+export default DrAdmin
