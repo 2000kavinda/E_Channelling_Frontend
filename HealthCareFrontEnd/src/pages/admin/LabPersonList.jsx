3cc4226a342fbd350d02a808acc2b3eff74b507a
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2024-09-04 00:49:11 +0530

    Create Admin All Lab service and connect to frontend

--- HealthCareFrontEnd/src/pages/admin/LabPersonList.jsx
+++ HealthCareFrontEnd/src/pages/admin/LabPersonList.jsx
@@ -2,12 +2,17 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { FaQuestion } from "react-icons/fa6";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useRef } from 'react';
+import { useState, useEffect } from "react";
 import DoctorPicture from '../../assets/Images/Ellipse34.png';
 import { useNavigate } from 'react-router-dom';
+import { AllLabPersonList } from '../../service/AllLabPersonListService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function LabPersonList() {
     const divRef = useRef(null);
     const bottomRef = useRef(null);
+    const [labPerson, setLabPerson] = useState([]);
 
     const navigate = useNavigate();
 
@@ -15,8 +20,27 @@ function LabPersonList() {
         navigate('/AddLabPerson');
     };
 
+    useEffect(() => {
+        const toastId = 'unique-toast-id';
+        AllLabPersonList()
+            .then((response) => {
+                console.log(response.data);
+                setLabPerson(response.data);
+                if (!toast.isActive(toastId)) {
+                    // toast.success('Registration successful!', { toastId });
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                if (!toast.isActive(toastId)) {
+                    toast.error('No Schedules available!', { toastId });
+                }
+            });
+    }, []);
+
     return (
         <div className="flex flex-col px-10 pt-10">
+            <ToastContainer />
             <div className="flex flex-row justify-between w-full">
                 {/* Greeting message */}
                 <div className="flex flex-col">
@@ -75,152 +99,45 @@ function LabPersonList() {
                         style={{ overflowY: 'scroll', height: '480px' }}
                     >
 
-                        {/* Card */}
-                        <div className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3">
-                            <div className="flex flex-row items-center h-full gap-8">
-                                {/* Profile Picture */}
-                                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
-                                    <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
-                                </div>
-                                {/* Patient Details */}
-                                <div className="flex flex-col">
-                                    <div className="text-lg font-semibold text-[#666767]">Mr.Hiran Welagedara</div>
-                                    <div className="flex flex-row gap-4 text-sm text-[#666767]">
-                                        <div className="flex flex-row">
-                                            <div className="pr-1">Dr.Hiran Welcome </div>
+                        {
+                            labPerson && labPerson.length > 0 ? (
+                                labPerson.map((LabPersonList) => (
+                                    <div
+                                        key={LabPersonList.lPRegNo}
+                                        className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3"
+                                    >
+                                        <div className="flex flex-row items-center h-full gap-8">
+                                            {/* Profile Picture */}
+                                            <div className="flex w-[70px] h-[70px] bg-black rounded-full">
+                                                <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
+                                            </div>
+                                            {/* Patient Details */}
+                                            <div className="flex flex-col">
+                                                <div className="text-lg font-semibold text-[#666767]">{LabPersonList.lPName}</div>
+                                                <div className="flex flex-row gap-4 text-sm text-[#666767]">
+                                                    <div className="flex flex-row gap-1">
+                                                        <div className="pr-1">Lab No: </div>
+                                                        <div className="pr-1">{LabPersonList.labNo}</div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="flex flex-row text-sm text-[#666767] gap-1">
+                                                    <div className="pr-1">Qualifications:</div>
+                                                    <div className="pr-1">{LabPersonList.lPQualification}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="flex flex-row gap-5">
+                                            <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
+                                            <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
                                         </div>
                                     </div>
-
-                                    <div className="flex flex-row text-sm text-[#666767]">
-                                        <div className="pr-1">Eye Surgeon</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-row gap-5">
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
-                            </div>
-                        </div>
-
-
-
-                        {/* Card */}
-                        <div className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3">
-                            <div className="flex flex-row items-center h-full gap-8">
-                                {/* Profile Picture */}
-                                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
-                                    <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
-                                </div>
-                                {/* Patient Details */}
-                                <div className="flex flex-col">
-                                    <div className="text-lg font-semibold text-[#666767]">Mr.Hiran Welagedara</div>
-                                    <div className="flex flex-row gap-4 text-sm text-[#666767]">
-                                        <div className="flex flex-row">
-                                            <div className="pr-1">Dr.Hiran Welcome </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="flex flex-row text-sm text-[#666767]">
-                                        <div className="pr-1">Eye Surgeon</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-row gap-5">
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
-                            </div>
-                        </div>
-
-
-                        {/* Card */}
-                        <div className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3">
-                            <div className="flex flex-row items-center h-full gap-8">
-                                {/* Profile Picture */}
-                                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
-                                    <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
-                                </div>
-                                {/* Patient Details */}
-                                <div className="flex flex-col">
-                                    <div className="text-lg font-semibold text-[#666767]">Mr.Hiran Welagedara</div>
-                                    <div className="flex flex-row gap-4 text-sm text-[#666767]">
-                                        <div className="flex flex-row">
-                                            <div className="pr-1">Dr.Hiran Welcome </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="flex flex-row text-sm text-[#666767]">
-                                        <div className="pr-1">Eye Surgeon</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-row gap-5">
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
-                            </div>
-                        </div>
-
-
-
-                        {/* Card */}
-                        <div className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3">
-                            <div className="flex flex-row items-center h-full gap-8">
-                                {/* Profile Picture */}
-                                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
-                                    <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
-                                </div>
-                                {/* Patient Details */}
-                                <div className="flex flex-col">
-                                    <div className="text-lg font-semibold text-[#666767]">Mr.Hiran Welagedara</div>
-                                    <div className="flex flex-row gap-4 text-sm text-[#666767]">
-                                        <div className="flex flex-row">
-                                            <div className="pr-1">Dr.Hiran Welcome </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="flex flex-row text-sm text-[#666767]">
-                                        <div className="pr-1">Eye Surgeon</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-row gap-5">
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
-                            </div>
-                        </div>
-
-
-
-                        {/* Card */}
-                        <div className="w-full h-[100px] bg-white rounded-lg flex flex-row justify-between px-4 items-center mb-3">
-                            <div className="flex flex-row items-center h-full gap-8">
-                                {/* Profile Picture */}
-                                <div className="flex w-[70px] h-[70px] bg-black rounded-full">
-                                    <img src={DoctorPicture} alt="ProfileImage" className="w-full h-full" />
-                                </div>
-                                {/* Patient Details */}
-                                <div className="flex flex-col">
-                                    <div className="text-lg font-semibold text-[#666767]">Mr.Hiran Welagedara</div>
-                                    <div className="flex flex-row gap-4 text-sm text-[#666767]">
-                                        <div className="flex flex-row">
-                                            <div className="pr-1">Dr.Hiran Welcome </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="flex flex-row text-sm text-[#666767]">
-                                        <div className="pr-1">Eye Surgeon</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-row gap-5">
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#005F7E]">Edit</button>
-                                <button className="px-10 rounded-lg text-white text-sm font-medium py-2 bg-[#FF6464]">Delete</button>
-                            </div>
-                        </div>
+                                ))
+                            ) : (
+                                <div className="flex flex-col justify-center w-full h-full text-lg text-center text-[#005F7E] font-semibold">No schedules available</div>
+                            )
+                        }
 
                         <div ref={bottomRef}></div>
                     </div>
