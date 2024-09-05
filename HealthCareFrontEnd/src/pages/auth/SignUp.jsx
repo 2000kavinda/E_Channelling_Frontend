f42a4ae16ca37ee060054cf915316c6ef3e267f7
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2024-09-06 01:14:00 +0530

    Sign Up Loading Animation and Automatic Navigation create

--- HealthCareFrontEnd/src/pages/auth/SignUp.jsx
+++ HealthCareFrontEnd/src/pages/auth/SignUp.jsx
@@ -3,17 +3,12 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import ImageUpload from '../../components/admin/ImageUpload';
 import { useState } from "react";
 import axios from 'axios';
+import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner';
 
 function SignUp() {
     const navigate = useNavigate();
-
-    const handleAddNewClick = () => {
-        setTimeout(() => {
-            navigate('/SignIn');
-        }, 300); // Adjust the delay time in milliseconds (300ms in this example)
-    };
-
     const [url, setUrl] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const [formData, setFormData] = useState({
         regNo: '',
@@ -25,51 +20,59 @@ function SignUp() {
         gender: "",
         contact: "",
     });
+
     const handleChange = async (event) => {
         const { name, value } = event.target;
         setFormData({ ...formData, [name]: value });
-    }
+    };
 
     const REST_API_BASE_URL = `http://localhost:8081/api/v1/auth/register`;
 
-
-
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setLoading(true);
 
-        // set url
-        const photoUrl = localStorage.getItem('url')
-
-        const updatedFormData = { ...formData, pprofileImage: photoUrl }
+        const photoUrl = localStorage.getItem('url');
+        const updatedFormData = { ...formData, pprofileImage: photoUrl };
 
         try {
             const response = await axios.post(REST_API_BASE_URL, updatedFormData);
             console.log("API response:", response);
-            alert("Successfully Registered!");
+            // alert("Successfully Registered!");
+            navigate('/SignIn');
+            setLoading(false);
         } catch (error) {
             console.error("There was an error making the request:", error);
+            setLoading(false);
         }
-    }
+    };
 
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen px-16 py-10">
-            <div className="flex flex-row items-center justify-start w-full gap-8 pb-5 ">
-                <button className="w-[120px] py-2 bg-[#005F7E] text-white rounded-lg flex flex-row items-center gap-1 font-semibold text-lg justify-center" onClick={handleAddNewClick}>
+        <div className="relative flex flex-col items-center justify-center w-screen h-screen px-16 py-10">
+            {/* Back Button */}
+            <div className="flex flex-row items-center justify-start w-full gap-8 pb-5">
+                <button className="w-[120px] py-2 bg-[#005F7E] text-white rounded-lg flex flex-row items-center gap-1 font-semibold text-lg justify-center" onClick={() => navigate('/SignIn')}>
                     <IoMdArrowRoundBack />
                     <div>Back</div>
                 </button>
                 <div className='flex flex-row text-4xl font-bold text-[#005F7E]'>Sign up</div>
             </div>
 
-            <div className='flex flex-col w-full h-full bg-[#c3e9f6] rounded-xl py-10 px-10'>
+            {/* Form Container */}
+            <div className='relative flex flex-col w-full h-full bg-[#c3e9f6] rounded-xl py-10 px-10'>
+
+                {/* Loading Overlay */}
+                {loading && (
+                    <div className="absolute inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75">
+                        <LoadingSpinner />
+                    </div>
+                )}
+
                 <ImageUpload setUrl={setUrl} url={url} />
 
+                {/* Form */}
                 <form onSubmit={handleSubmit}>
-
-
-                    {/* Text Boxes */}
                     <div className="flex flex-col items-center gap-5 pt-10">
-                        {/* Input Row */}
                         <div className="flex flex-row gap-16">
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Patient name</div>
@@ -80,7 +83,6 @@ function SignUp() {
                                     value={formData.pname}
                                     onChange={handleChange} />
                             </div>
-
 
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Register number</div>
@@ -94,7 +96,6 @@ function SignUp() {
                             </div>
                         </div>
 
-                        {/* Input Row */}
                         <div className="flex flex-row gap-16">
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Email address</div>
@@ -105,7 +106,6 @@ function SignUp() {
                                     value={formData.email}
                                     onChange={handleChange} />
                             </div>
-
 
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Birthday</div>
@@ -119,7 +119,6 @@ function SignUp() {
                             </div>
                         </div>
 
-                        {/* Input Row */}
                         <div className="flex flex-row gap-16">
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Gender</div>
@@ -131,7 +130,6 @@ function SignUp() {
                                     onChange={handleChange}
                                 />
                             </div>
-
 
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Contact number</div>
@@ -145,7 +143,6 @@ function SignUp() {
                             </div>
                         </div>
 
-                        {/* Input Row */}
                         <div className="flex flex-row justify-center w-full gap-16">
                             <div className="flex flex-col gap-1">
                                 <div className="text-normal text-[#1b5f75] font-semibold">Password</div>
@@ -169,20 +166,17 @@ function SignUp() {
 
                         <div className="flex flex-row justify-center w-full gap-16 pt-10">
                             <div className="flex flex-row items-center justify-center gap-10">
-                                <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold">Cancel</button>
-                                <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold">Submit</button>
+                                <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold" disabled={loading}>Cancel</button>
+                                <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold" disabled={loading}>
+                                    Submit
+                                </button>
                             </div>
                         </div>
                     </div>
-
                 </form>
             </div>
-
-            
-                
-                
         </div>
-    )
+    );
 }
 
 export default SignUp;
