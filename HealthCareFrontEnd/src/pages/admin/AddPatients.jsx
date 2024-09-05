f268ed95f3c6744ba3c4d39779ab07eaef686593
nsewmini <nsewmini2001@gmail.com>
2024-09-05 20:16:58 +0530

    to resolve the conflicts

---
+++ HealthCareFrontEnd/src/pages/admin/AddPatients.jsx
@@ -0,0 +1,111 @@
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
+import ImageUpload from "../../components/admin/ImageUpload";
+
+function AddPatients() {
+    const navigate = useNavigate();
+
+    const handleAddNewClick = () => {
+        navigate('/AdminSideBar');
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-[900px] py-10 bg-white">
+            <div className="flex flex-row justify-start w-full gap-8 px-16 pb-5">
+                <button className="w-[120px] py-2 bg-[#005F7E] text-white rounded-lg flex flex-row items-center gap-1 font-semibold text-lg justify-center" onClick={handleAddNewClick}>
+                    <IoMdArrowRoundBack />
+                    <div>Back</div>
+                </button>
+                <div className="text-3xl font-bold text-[#005F7E]">Fill the details</div>
+            </div>
+
+            {/* Details Pages */}
+            <div className="w-11/12 h-[800px] bg-[#c9e2eb] rounded-lg px-10 py-10 flex flex-col">
+
+                {/* Profile Picture */}
+                <ImageUpload />
+
+
+                {/* Text Boxes */}
+                <div className="flex flex-col items-center gap-5 pt-10">
+                    {/* Input Row */}
+                    <div className="flex flex-row gap-16">
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">First name</div>
+                            <input type="text"
+                                placeholder="First name..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+
+
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Second name</div>
+                            <input type="text"
+                                placeholder="Second name..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+                    </div>
+
+                    {/* Input Row */}
+                    <div className="flex flex-row gap-16">
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Age</div>
+                            <input type="text"
+                                placeholder="Registration number..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+
+
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Height</div>
+                            <input type="text"
+                                placeholder="NIC number..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+                    </div>
+
+                    {/* Input Row */}
+                    <div className="flex flex-row gap-16">
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Weight</div>
+                            <input type="text"
+                                placeholder="Age..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+
+
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Doctor</div>
+                            <input type="text"
+                                placeholder="Graduate from..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[400px] h-[45px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+                    </div>
+
+                    {/* Input Row */}
+                    <div className="flex flex-row justify-center w-full gap-16">
+                        <div className="flex flex-col gap-1">
+                            <div className="text-normal text-[#1b5f75] font-semibold">Diseases details</div>
+                            <input type="text"
+                                placeholder="Age..."
+                                className="px-4 py-2 bg-[#f1f1f1] text-gray-800 text-sm w-[860px] h-[120px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00394C]" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-row justify-center w-full gap-16 pt-10">
+                        <div className="flex flex-row items-center justify-center gap-10">
+                            <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold">Cancel</button>
+                            <button className="w-[180px] h-[50px] bg-[#1b5f75] rounded-xl text-white text-normal font-semibold">Submit</button>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default AddPatients
---
+++ HealthCareFrontEnd/src/pages/admin/AdminSideBar.jsx
@@ -0,0 +1,103 @@
+import { TbLogout2 } from "react-icons/tb";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { IoPeopleOutline } from "react-icons/io5";
+import { useState } from 'react';
+import AdminDashboard from "./AdminDashboard";
+import Logo from '../../assets/Images/Logo.png';
+import PatientsList from "./PatientsList";
+import DoctorList from "./DoctorList";
+import LabPersonList from "./LabPersonList";
+import ScheduleList from "./ScheduleList";
+
+function AdminSideBar() {
+
+    const [activePage, setActivePage] = useState('dashboard');
+    
+    return (
+        <div className="flex flex-row flex-1 w-screen h-screen">
+            <div className="flex flex-col justify-start w-1/4 h-full bg-[#00394C] items-center ">
+                {/* Profile Picture */}
+                <div className="flex w-full h-[340px] bg-[#D9F2EF] flex-col justify-center items-center">
+                    <img src={Logo} width={180} height={150}/>
+                </div>
+
+                {/* Navigation buttons */}
+                <div className='flex flex-col w-full gap-4 pt-16 pl-4'>
+                    {/* Dashboard Button */}
+                    <button
+                        className={`flex flex-row items-center h-[60px] w-full justify-center rounded-l-full pr-4 ${activePage === 'dashboard' ? 'bg-[#E6F2F6] text-[#00394C]' : 'bg-[#00394C] text-[#EEEEEE]'
+                            } hover:bg-[#E6F2F6] hover:text-[#00394C]`}
+                        onClick={() => setActivePage('dashboard')}
+                    >
+                        <div><MdOutlineSpaceDashboard className='w-8 h-8' /></div>
+                        <div className='pl-4 text-lg font-semibold'>Dashboard</div>
+                    </button>
+
+                    {/* Schedules buttons */}
+                    <button
+                        className={`flex flex-row items-center h-[60px] w-full justify-center rounded-l-full pr-4 font-semibold ${activePage === 'schedule' ? 'bg-[#E6F2F6] text-[#00394C]' : 'bg-[#00394C] text-[#EEEEEE]'
+                            } hover:bg-[#E6F2F6] hover:text-[#00394C]`}
+                        onClick={() => setActivePage('schedule')}
+                    >
+                        <div className='w-8'><RiCalendarScheduleLine className='w-8 h-8' /></div>
+                        <div className='pl-4 text-lg'>Schedules</div>
+                    </button>
+
+                    {/* Patients List buttons */}
+                    <button
+                        className={`flex flex-row items-center h-[60px] w-full justify-center rounded-l-full pr-4 font-semibold ${activePage === 'patients' ? 'bg-[#E6F2F6] text-[#00394C]' : 'bg-[#00394C] text-[#EEEEEE]'
+                            } hover:bg-[#E6F2F6] hover:text-[#00394C]`}
+                        onClick={() => setActivePage('patients')}
+                    >
+                        <div className='w-8'><IoPeopleOutline className='w-8 h-8' /></div>
+                        <div className='pl-4 text-lg'>Patients List</div>
+                    </button>
+
+                    {/* Patients List buttons */}
+                    <button
+                        className={`flex flex-row items-center h-[60px] w-full justify-center rounded-l-full pr-4 font-semibold ${activePage === 'doctors' ? 'bg-[#E6F2F6] text-[#00394C]' : 'bg-[#00394C] text-[#EEEEEE]'
+                            } hover:bg-[#E6F2F6] hover:text-[#00394C]`}
+                        onClick={() => setActivePage('doctors')}
+                    >
+                        <div className='w-8'><IoPeopleOutline className='w-8 h-8' /></div>
+                        <div className='pl-4 text-lg'>Doctors List</div>
+                    </button>
+
+                    {/* Patients List buttons */}
+                    <button
+                        className={`flex flex-row items-center h-[60px] w-full justify-center rounded-l-full pr-4 font-semibold ${activePage === 'labs' ? 'bg-[#E6F2F6] text-[#00394C]' : 'bg-[#00394C] text-[#EEEEEE]'
+                            } hover:bg-[#E6F2F6] hover:text-[#00394C]`}
+                        onClick={() => setActivePage('labs')}
+                    >
+                        <div className='w-8'><IoPeopleOutline className='w-8 h-8' /></div>
+                        <div className='pl-4 text-lg'>Labs person List</div>
+                    </button>
+
+                </div>
+
+                {/* Log out Button */}
+                <div className='flex flex-col items-center justify-end w-full h-full'>
+                    <button className='w-full flex flex-row h-[60px] border-top border-[#D9D9D9] border-t-2 justify-center items-center text-white font-semibold hover:bg-[#E6F2F6] hover:text-[#00394C]'>
+                        <div className='w-8'><TbLogout2 className='w-6 h-6' /></div>
+                        <div className='pl-2 text-lg'>Logout</div>
+
+                    </button>
+                </div>
+            </div>
+
+
+            {/* Screen */}
+            <div className='w-3/4 h-full'>
+                {activePage === 'dashboard' && <div><AdminDashboard /></div>}
+                {activePage === 'schedule' && <div><ScheduleList /></div>}
+                {activePage === 'patients' && <div><PatientsList /></div>}
+                {activePage === 'doctors' && <div><DoctorList /></div>}
+                {activePage === 'labs' && <div><LabPersonList /></div>}
+            </div>
+
+        </div>
+    )
+}
+
+export default AdminSideBar
