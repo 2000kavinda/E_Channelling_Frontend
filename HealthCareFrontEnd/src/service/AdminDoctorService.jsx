540e81e8ca31c8ca5308475e8c1c3f8cba232b25
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2024-09-05 07:38:27 +0530

    Change the port numbers

--- HealthCareFrontEnd/src/service/AdminDoctorService.jsx
+++ HealthCareFrontEnd/src/service/AdminDoctorService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define the base URL for the API
-const REST_API_BASE_URL = "http://localhost:8080/api/v1/doctor";
+const REST_API_BASE_URL = "http://localhost:8088/api/v1/doctor";
 
 // Function to get the list of all lab persons
 export const AllDoctorsList = () => {
--- HealthCareFrontEnd/src/service/PatientsBookingServices.jsx
+++ HealthCareFrontEnd/src/service/PatientsBookingServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = "http://localhost:8088/api/v1/booking/filterByDate";
+const REST_API_BASE_URL = "http://localhost:8080/api/v1/booking/filterByDate";
 
 // Modify listSchedules to accept drRegNo as a parameter
 export const listPatients = (drRegNo,date) => {
